7d0c952d7cec2b0b9ba14a21e9f083ec7668f956
Ivan Guerrero <iguerrero@compusoluciones.com>
2022-10-03 12:01:18 -0500

    punto tres

--- src/user.js
+++ src/user.js
@@ -1,9 +1,37 @@
-class User {
-    constructor(nombre,apellido,libros,mascota){
+export class User {
+
+    constructor(nombre,apellido,libros,mascotas){
         this.nombre = nombre;
         this.apellido = apellido;
         this.libros = libros;
-        this.mascotas = mascota
+        this.mascotas = mascotas
+    }
 
+    addMascota(mascota){
+        this.mascotas.push(mascota)
+    }
+
+    addBook(nombre,autor){
+
+        const libro = {
+            'nombre': nombre,
+            'autor': autor
+        }
+
+        this.libros.push(libro)
+    }
+
+    getFullName() {
+        return `${this.nombre} ${this.apellido}`
+    }
+
+    getBookNames(){
+        return this.libros.map(book => {
+            return book.nombre
+        })
+    }
+
+    countMascotas(){
+        return this.mascotas.length
     }
 }
